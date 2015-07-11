b5b049818332dea2bd500a6cf83b8add3b5b430e
Nikita Kuznetsov <me@daedalus.ru>
2015-07-11 15:47:18 +0300

    #fix, in chat username is optional type, also check first_name

--- telegram/message.py
+++ telegram/message.py
@@ -61,15 +61,14 @@ class Message(object):
         else:
             from_user = None
 
+        chat = None
         if 'chat' in data:
-            if 'username' in data['chat']:
+            if 'username' in data['chat'] or 'first_name' in data['chat']:
                 from telegram import User
                 chat = User.de_json(data['chat'])
             if 'title' in data['chat']:
                 from telegram import GroupChat
                 chat = GroupChat.de_json(data['chat'])
-        else:
-            chat = None
 
         if 'forward_from' in data:
             from telegram import User
